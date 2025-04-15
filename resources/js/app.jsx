a41bda6aaf72ce97874cce36076b729a6b81230e
cutiep4nda <haidaragung16@gmail.com>
2025-04-15 15:16:10 +0700

    installation

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+        root.render(<App {...props} />);
+    },
+});
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.jsx'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    ssr: {
+        noExternal: ['@inertiajs/server']
+    },
+});
