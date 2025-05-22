d005337fa309be813e44d4f7f3ca1622b4d66e7c
cutiep4nda <haidaragung16@gmail.com>
2025-05-22 10:21:11 +0700

    update: filter

--- app/Http/Controllers/KostController.php
+++ app/Http/Controllers/KostController.php
@@ -36,6 +36,65 @@ class KostController extends Controller
         return Inertia::render("Semua", ['kost' => $data]);
     }
     public function filter(Request $request){
-        dd($request);
+        $data = $request->all();
+        $search = $data['search'];
+        $gender = $data['gender'];
+        
+        $min_price = 0;
+        if($data['min_price'] != 0){
+            $min_price = $data['min_price'];
+        }
+        $max_price = 10000000;
+        if($data['max_price'] != 0){
+            $max_price = $data['max_price'];
+        }
+        
+        $facilities = [];
+        foreach($data['facilities'] as $value){
+            if($value == "K. Mandi Dalam"){
+                $facilities = array_merge($facilities, ['km_dalam']);
+            }
+            if($value == "AC"){
+                $facilities = array_merge($facilities, ['ac']);
+            }
+            if($value == "Lemari"){
+                $facilities = array_merge($facilities, ['lemari']);
+            }
+            if($value == "Wifi"){
+                $facilities = array_merge($facilities, ['wifi']);
+            }
+            if($value == "Kipas"){
+                $facilities = array_merge($facilities, ['kipas_angin']);
+            }
+            if($value == "Kasur"){
+                $facilities = array_merge($facilities, ['kasur']);
+            }
+        }
+        
+        $kost = new Kost();
+        foreach($facilities as $value){
+            $kost = $kost->where($value, 1);
+        }
+        if($data['gender'] != null){
+            $kost = $kost->whereRelation('jenis', 'jenis', $gender);
+        }
+        $kost = $kost->whereBetween('harga', [$min_price, $max_price]);
+        
+        if($data['search'] != ''){
+            $kost = $kost->where('nama', 'like', "%{$search}%")->orWhere('deskripsi', 'like', "%{$search}%");
+        }
+
+        $kost = $kost->get();
+        //$kost = $kost->toArray();
+        $data = [];
+        foreach ($kost as $key => $value) {
+            $data[] = array_merge($kost[$key]->toArray(), array(
+                'jenis' => $value->jenis->jenis,
+                'daerah' => $value->daerah->daerah,
+                'pemilik' => $value->pemilik->nama_pemilik
+            ));
+        }
+
+        dd($data);
     }
 }
